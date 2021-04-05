152c0581aecf970642d51c50e9ef33bb28638516
angelicavc7 <angelicavillar_c@live.com>
2021-04-04 19:36:41 -0700

    actual commit

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+//show todays current date
+today = moment();
+    $("#currentDay").text(today.format("dddd, MMM Do"));
+
+currentHour = moment().format("hh:mm:ss");
+console.log(currentHour)
+
+
+var timeBlock = document.querySelectorAll("#time-block")
+var timeStart = document.querySelector("#9")
+var timeEnd = document.querySelector("#5")
+
+
+
+//var timeRange = moment.timeRange(startTime, endTime)
+//current timer appearing on console.log
+
+//9 am to 5pm display past, present, current time
+
+
+
+
+ 
+       
+        
+
+
+//show hours with past, present, future colors
+
+
+
+
+
+
+//timeRange = moment().range(times);
+
+
+
+
+//var time inputs = json.parse(localStorage.getItem('timeInputs')) {}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   // saveBtn.addEventListener("click", function())
