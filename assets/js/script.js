ae0d1906dc97bce224a6acf194f01a7d927a3b11
angelicavc7 <angelicavillar_c@live.com>
2021-05-25 16:34:07 -0700

    timeblocks bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,24 @@ today = moment();
     $("#currentDay").text(today.format("dddd, MMM Do"));
 
 currentHour = moment().format("hh:mm:ss");
-console.log(currentHour)
+console.log(currentHour);
 
 
-var timeBlock = document.querySelectorAll("#time-block")
-var timeStart = document.querySelector("#9")
-var timeEnd = document.querySelector("#5")
-
-
+var timeBlock = document.querySelectorAll("#time-block");
+//var timeStart = document.querySelector("#9");
+//var timeEnd = document.querySelector("#5");
+var row = document.querySelector("#row");
+var textAreaInput = document.querySelector("#description");
 
 //var timeRange = moment.timeRange(startTime, endTime)
 //current timer appearing on console.log
 
 //9 am to 5pm display past, present, current time
 
-
-
+var description = {
+    textArea: textAreaInput.value,
+}
+console.log(description);
 
  
        
